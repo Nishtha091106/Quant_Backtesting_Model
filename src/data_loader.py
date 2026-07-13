b8cfaae348a346ac4f78419bd78de18cdd0215cd
Nishtha Tripathy <nishthatripathy@Nishthas-MacBook-Air.local>
2026-07-14 01:28:43 +0530

    Implement data loader module

---
+++ src/data_loader.py
@@ -0,0 +1,42 @@
+import yfinance as yf
+import pandas as pd
+
+def download_stock_data(
+        tickers,
+        start_date,
+        end_date
+):
+    """
+    Downloads historical stock data
+    Saves it as a CSV file
+    returns a Dataframe
+    
+    """
+
+    try:
+
+        data=yf.download(
+            tickers=tickers,
+            start=start_date,
+            end=end_date
+        )
+
+        if data.empty:
+            print("Error: invalid ticker symbol")
+            return None
+        
+        filename=f"data/raw/{tickers}_{start_date}_{end_date}.csv"
+        data.to_csv(filename)
+
+        return data
+
+    except Exception as e :
+        print(f"Error occured: {e}")
+        return None
+    
+if __name__ == "__main__":
+    download_stock_data(
+        tickers="AAPL",
+        start_date="2024-01-01",
+        end_date="2025-01-01"
+    )
